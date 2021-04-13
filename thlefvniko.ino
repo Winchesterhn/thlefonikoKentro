bf8a71d6b464c6a9229bbd87f44fd7b36fea7f6c
Winchesterhn <joan13sn@gmail.com>
2021-04-14 00:37:15 +0300

    Update thlefvniko.ino
    added the cable tables and the cable connection check. If its connected stays lit.

--- thlefvniko.ino
+++ thlefvniko.ino
@@ -3,7 +3,7 @@
 int ledBoard[5] = {2, 3, 4, 5, 6}; //declare led pins
 int lightTimesBoard[5] = {3, 8, 6, 9, 1};//declare how many times each led has to light up
 int UpJackBoard[5] = {8, 9, 10, 11, 12};
-int DownJackBoard[5] = {A0, A2, A3, A4, A5};
+int DownJackBoard[5] = {A0, A1, A2, A3, A4};
 bool CorrectConnection[5] = {0, 0, 0, 0, 0};
 
 void setup() {
@@ -14,52 +14,37 @@ void setup() {
     pinMode(UpJackBoard[i], OUTPUT);
     pinMode(DownJackBoard[i], INPUT);
   }
-  Serial.begin(9600);
+
 
 }
 
 void loop() {
 
-  for (int i = 0; i < 5; i = i + 1)// reset
+  for (int i = 0; i < 5; i = i + 1)// blinking. Puzzle reset
   {
     blinkLED(ledBoard[i], 800);
   }
-  bool correctConx;
-  int i = 0;
-  blinkCount(ledBoard[i], lightTimesBoard[i]);
-  digitalWrite(UpJackBoard[i], HIGH);
-  
-  unsigned long time_start = millis();// start time
-  Serial.println("start timer");
-  while (millis() - time_start <= 3000) {
-    correctConx = digitalRead (DownJackBoard[i]);
-    Serial.println(correctConx);
-  }
-  Serial.println("end timer");
-  if (correctConx == 1) {
-    CorrectConnection[i] = 1;
-    digitalWrite(ledBoard[i], HIGH);
-    delay (3000);
-  }
 
+  for (int i = 0; i < 5; i = i + 1) {
+
+    blinkCount(ledBoard[i], lightTimesBoard[i]);
+    digitalWrite(UpJackBoard[i], HIGH);
+
+    unsigned long time_start = millis();// start time
+
+    while (millis() - time_start <= 3000) {
+      CorrectConnection[i] = digitalRead (DownJackBoard[i]);
+    }
+
+    if ( CorrectConnection[i] == 1) {
+      //CorrectConnection[i] = 1;
+      digitalWrite(ledBoard[i], HIGH);
+      delay(3000);
+    }
+  }
 }
-//
-//  for (int i = 0; i < 5; i = i + 1)
-//  {
-//    blinkCount(ledBoard[i], lightTimesBoard[i]);
-//  }
-//  for (int i = 0; i < 5; i = i + 1)
-//  {
-//    if (DownJackBoard[i] == UpJackBoard[i]) {
-//      digitalWrite(ledBoard[i], HIGH);
-//    }
-//  }
 
-
-
-// blink the LED on the given pin for the duration in milliseconds
-
-void blinkLED(int pin, int duration)
+void blinkLED(int pin, int duration) // blink the LED on the given pin for the duration in milliseconds
 {
   digitalWrite(pin, HIGH); // turn LED on
   delay(duration);
@@ -70,11 +55,10 @@ void blinkLED(int pin, int duration)
 void blinkCount(int pin, int times)
 {
 
-  for (int i = 1; i <= times; i = i + 1)
+  for (int i = 1; i <= times; i = i + 1)// blinks each led as manny times as it is declared on the lightTimesBoard
   {
     blinkLED(pin, 800);
     delay(800);
-
   }
 
 }
