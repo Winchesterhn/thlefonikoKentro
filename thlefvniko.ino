9dfdfafe8d0b8c5ad02186379890ff85845bbec7
Winchesterhn <joan13sn@gmail.com>
2021-04-14 00:04:59 +0300

    Update thlefvniko.ino

--- thlefvniko.ino
+++ thlefvniko.ino
@@ -1,33 +1,61 @@
 
 
-int ledBoard[5] = {2, 3, 4, 5, 6};
-int lightTimesBoard[5] = {3, 8, 6, 9, 1};
+int ledBoard[5] = {2, 3, 4, 5, 6}; //declare led pins
+int lightTimesBoard[5] = {3, 8, 6, 9, 1};//declare how many times each led has to light up
+int UpJackBoard[5] = {8, 9, 10, 11, 12};
+int DownJackBoard[5] = {A0, A2, A3, A4, A5};
+bool CorrectConnection[5] = {0, 0, 0, 0, 0};
 
 void setup() {
   // put your setup code here, to run once:
   for (int i = 0; i < 5; i = i + 1)
   {
     pinMode(ledBoard[i], OUTPUT); // declare LED pins as output
+    pinMode(UpJackBoard[i], OUTPUT);
+    pinMode(DownJackBoard[i], INPUT);
   }
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // flash each of the LEDs for 1000 milliseconds (1 second)
 
-  for (int i = 0; i < 5; i = i + 1)
+  for (int i = 0; i < 5; i = i + 1)// reset
   {
     blinkLED(ledBoard[i], 800);
   }
-
-  for (int i = 0; i < 5; i = i + 1)
-  {
-    blinkCount(ledBoard[i], lightTimesBoard[i]);
+  bool correctConx;
+  int i = 0;
+  blinkCount(ledBoard[i], lightTimesBoard[i]);
+  digitalWrite(UpJackBoard[i], HIGH);
+  
+  unsigned long time_start = millis();// start time
+  Serial.println("start timer");
+  while (millis() - time_start <= 3000) {
+    correctConx = digitalRead (DownJackBoard[i]);
+    Serial.println(correctConx);
+  }
+  Serial.println("end timer");
+  if (correctConx == 1) {
+    CorrectConnection[i] = 1;
+    digitalWrite(ledBoard[i], HIGH);
+    delay (3000);
   }
 
-
 }
+//
+//  for (int i = 0; i < 5; i = i + 1)
+//  {
+//    blinkCount(ledBoard[i], lightTimesBoard[i]);
+//  }
+//  for (int i = 0; i < 5; i = i + 1)
+//  {
+//    if (DownJackBoard[i] == UpJackBoard[i]) {
+//      digitalWrite(ledBoard[i], HIGH);
+//    }
+//  }
+
+
 
 // blink the LED on the given pin for the duration in milliseconds
 
