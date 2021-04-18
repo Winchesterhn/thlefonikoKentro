581e4a31f8058693f5655b7e68b32dd33edae7d3
CptAwe <georgepapoulias@gmail.com>
2021-04-18 21:15:10 +0300

    initial commit. Made the initial changes to make the program more responsive. Added a function for checking each plug and remembering the earliest in the queue, wrongly connected one.

--- thlefvniko.ino
+++ thlefvniko.ino
@@ -1,119 +1,134 @@
+/**
+ * There are 5 male 3.5mm jacks. They have to be plugged into 5 out of 20 female ones.
+ * Each female one has a number above it from 1 to 20.
+ * 
+ * There are 5 LEDs above each male jack. They flash a specific amount of times and
+ * indicate which male plug connects to which female one.
+ * 
+ * When all the jacks are connected successfully the riddle is solved
+ * 
+ * ---------------------
+ * 
+ * To test if a plug is connected correctly, all the others are connected to ground
+ * except one which is raised to 5V. If this 5V signal is detected in the correct female
+ * plug then it is connected correctly.
+ * 
+*/
+
+#define num_of_LEDs 5
 
 int ledBoard[5] = {2, 3, 4, 5, 6}; //declare led pins
 int lightTimesBoard[5] = {3, 2, 4, 1, 3 };//declare how many times each led has to light up
-int UpJackBoard[5] = {8, 9, 10, 11, 12};
-int DownJackBoard[5] = {A0, A1, A2, A3, A4};
-bool CorrectConnection[5] = {0, 0, 0, 0, 0};
+int hintBlinkDelay = 500;// How much time an LED will stay on or off
+
+int UpJackBoard[5] = {8, 9, 10, 11, 12};// The male plugs
+int DownJackBoard[5] = {A0, A1, A2, A3, A4};// The female plugs
+
+bool CorrectConnection[5] = {0, 0, 0, 0, 0};// Note if a plug is connected correctly
 bool puzzleSolved = false;
-int nDoor = {13};
+int nDoor = 13;// This pin is raised to 5V when the riddle is solved
 
 void setup() {
-  // put your setup code here, to run once:
+  
   for (int i = 0; i < 5; i = i + 1)
   {
     pinMode(ledBoard[i], OUTPUT); // declare LED pins as output
-    pinMode(UpJackBoard[i], OUTPUT);
-    pinMode(DownJackBoard[i], INPUT);
-    pinMode(nDoor, OUTPUT);
-    //    digitalWrite(nDoor, LOW);
+    pinMode(UpJackBoard[i], OUTPUT);// declare male plugs as output
+    pinMode(DownJackBoard[i], INPUT);// declare female plugs as input
   }
-  // 1 problem. Kai na moy sbhnei amesws otan to jesyndew(na dokimasw for mesa sth for alla poly xronoboro)
+
+  pinMode(nDoor, OUTPUT);
+  digitalWrite(nDoor, LOW);// pull the "riddle solved" pin to ground
+  
   Serial.begin(9600);
 }
 
+
+int times_the_LED_has_blinked = 0;// How many times the LED has already blinked
+bool LED_state = false;// If the LED is off or on
+unsigned long time_start;// How much time has passed since the LED has turned on or off
+
+
+// For which LED to show the hint. Basically, the first wrongly connected cable
+int what_hint_to_show = num_of_LEDs + 1;
 void loop() {
-
-  while (puzzleSolved = true) {
-    for (int i = 0; i < 5; i = i + 1) // blinking. Puzzle reset
-    {
-      CorrectConnection[i] = testConnection(i);
-      if (CorrectConnection[i] == 0) { //this if prevents the for puzzle reset loop from turning off the correctly connected light
-
-        digitalWrite(ledBoard[i], LOW);// if it got disconnected it turns it off
-        blinkLED(ledBoard[i], 500);
-        delay(1000);
-      }
-    }
-
-    for (int i = 0; i < 5; i = i + 1) {
-
-      CorrectConnection[i] = testConnection(i);
-      if (CorrectConnection[i] == 1) {//checks if connected??useful?
-
-        digitalWrite(ledBoard[i], HIGH);
-      }
-      else  //checks if disconnected
-      {
-        blinkCount(ledBoard[i], lightTimesBoard[i]);//puzzle hint
-
-        unsigned long time_start = millis();// start time
-
-        while (millis() - time_start <= 2000) {
-
-          CorrectConnection[i] = testConnection(i);
-
-        }
-        if ( CorrectConnection[i] == 1 ) {
-
-          digitalWrite(ledBoard[i], HIGH);
-          delay(1000);
-        }
-        else {
-          digitalWrite(ledBoard[i], LOW);
-
-        }
-
-      }
-
-    }
-
+  // Assume all plugs are connected correctly
+  what_hint_to_show = num_of_LEDs + 1;
+  
+  checkConnections();
+  
+  if (what_hint_to_show == num_of_LEDs + 1) {
+    // All plugs are correct, the riddle is solved
     Door();
-
-  }
-}
-void blinkLED(int pin, int duration) // blink the LED on the given pin for the duration in milliseconds
-{
-  digitalWrite(pin, HIGH); // turn LED on
-  delay(duration);
-  digitalWrite(pin, LOW); // turn LED off
-  delay(duration);
-}
-
-void blinkCount(int pin, int times)
-{
-
-  for (int i = 1; i <= times; i = i + 1)// blinks each led as manny times as it is declared on the lightTimesBoard
-  {
-    blinkLED(pin, 500);
-    delay(500);
-    CorrectConnection[i] = digitalRead (DownJackBoard[i]);//new check trial
   }
 
+  /*
+   * The riddle is not yet solved, show the appropriate hint.
+   * TODO: Remember in what state (on/off, time for flip) the LED is in.
+  */
+  if (times_the_LED_has_blinked == 0) {
+    // The LED has not blinked
+  }
+  time_start = millis();// start timer
+
 }
 
-bool testConnection(int cable) {// checks if the connection is correct. From where the 1 comes.
+
+/**
+ * Updates the CorrectConnection[] table by calling
+ * testConnection() for each cable. Also updates the
+ * state of the LEDs.
+ * 1 0 0 1 0 
+*/
+void checkConnections() {
+  for (int i=0; i<5; i++){
+    CorrectConnection[i] = testConnection(i);
+
+    if (CorrectConnection[i]) {
+      // Correctly connected, light it up
+      digitalWrite(ledBoard[i], HIGH);
+    } else {
+      // Wrongly connected
+
+      if (what_hint_to_show == i) {
+        // The hint for this LED is currently showing.
+        // Leave it alone, check the next connection
+        continue;
+      } else {
+        // This is an LED that perhaps was previously connected correctly
+        digitalWrite(ledBoard[i], LOW);
+      }
+
+      if (what_hint_to_show > i) {
+        // Remember the earliest plug that is not connected correctly
+        what_hint_to_show = i;
+      }
+    }
+  }
+}
+
+/**
+ * Checks if the connection is correct. From where the 1 comes.
+ * Pulls all wires to GND except the one that is to be tested.
+ * Then reads if the signal is detected in the correct plug.
+*/
+bool testConnection(int cable) {
   bool cableConnected;
   for (int i = 0; i < 5; i = i + 1) {
     digitalWrite(UpJackBoard[i], LOW);
   }
   digitalWrite(UpJackBoard[cable], HIGH);
-  cableConnected = digitalRead (DownJackBoard[cable]);
+  cableConnected = digitalRead(DownJackBoard[cable]);
   digitalWrite(UpJackBoard[cable], LOW);
   return cableConnected;
 }
 
-bool Door() {
-  for (int i = 0; i < 5; i = i + 1) {
-    digitalRead (CorrectConnection[i]);
-    if (CorrectConnection[i] == 0) {
-      puzzleSolved = false;
-      Serial.print("notSolved");
-    }
-    else {// ΔΕΝ ΞΕΡΩ ΓΙΑΤΙ ΑΛΛΑ ΔΕ ΜΠΑΙΝΕΙ ΠΟΤΕ ΕΔΩ
-      puzzleSolved = true;
-      return puzzleSolved;
-      digitalWrite(nDoor, HIGH);
-      Serial.print("SOLVED");
-    }
-  }
+/**
+ * Opens the door.
+ * For now it pulls the appropriate pin to 5V
+ * and pauses until full reset.
+*/
+void Door() {
+  digitalWrite(nDoor, HIGH);
+  while (true);
 }
